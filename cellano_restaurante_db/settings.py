94668bbd23bc37dec7df831d3e7855d6408f2a7f
HectorStiven <vigoyahector90@gmail.com>
2025-10-21 16:27:50 -0500

    Commit inicialaaaaaaa

--- cellano_restaurante_db/settings.py
+++ cellano_restaurante_db/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2#-(&3vdm2h=fmw^3+(&_pktzmal1me0kkzv!9$aaopjtc#w&t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
